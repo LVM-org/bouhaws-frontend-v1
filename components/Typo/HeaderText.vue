8aacde3666ebc2f185e1e88770ba9786d5ccf3bc
ArchyScript <yungdansa@gmail.com>
2023-06-18 15:40:36 +0100

    in progress

--- components/Typo/HeaderText.vue
+++ components/Typo/HeaderText.vue
@@ -1,18 +1,19 @@
 <template>
   <h3
-    :class="` font-bold 
-	${size == 'lg' ? 'lg:text-lg mdlg:text-lg text-sm' : ''}
-	${size == 'xl' ? 'lg:text-lg mdlg:text-lg text-lg' : ''}
-	${size == 'base' ? 'lg:text-base mdlg:text-[13px] text-sm' : ''}
-	${size == 'xs' ? 'mdlg:!text-[12px] text-xs' : ''}
-  ${size == '2xl' ? ' text-xl' : ''}
-  ${size == '3xl' ? ' text-2xl' : ''}
-	${customClass}
-	${color}`"
+    :class="`font-bold 
+    ${size == 'lg' ? 'lg:text-lg mdlg:text-lg text-sm' : ''}
+    ${size == 'xl' ? 'lg:text-lg mdlg:text-lg text-lg' : ''}
+    ${size == 'base' ? 'lg:text-base mdlg:text-[13px] text-sm' : ''}
+    ${size == 'xs' ? 'mdlg:!text-[12px] text-xs' : ''}
+    ${size == '2xl' ? ' text-xl' : ''}
+    ${size == '3xl' ? ' text-2xl' : ''}
+    ${customClass}
+    ${color}`"
   >
     <slot />
   </h3>
 </template>
+
 <script lang="ts">
 export default {
   props: {
